0ee7922148059c2dddee8fbb1149f994eb57f63a
Michael Manhard <mmanhard93@gmail.com>
2020-02-03 17:21:03 -0600

    Clean up view controller for "Choose Category" view per Swift 5.

--- frontend_code/Touche/Touche/ViewController_chooseCategory.swift
+++ frontend_code/Touche/Touche/ViewController_chooseCategory.swift
@@ -13,7 +13,6 @@
 import UIKit
 
 class ViewController_chooseCategory: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var bar: UIView!
@@ -27,19 +26,19 @@ class ViewController_chooseCategory: UIViewController, UITableViewDataSource, UI
     
     // MARK: UITableViewDataSource and UITableViewDelegate Methods
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) ->
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) ->
         Int{
             return self.categoryArray.count
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        var cell:TableViewCell = self.tableView.dequeueReusableCellWithIdentifier("categoryCell") as! TableViewCell;
+        let cell:TableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "categoryCell") as! TableViewCell;
         cell.titleLabel.text = self.categoryArray[indexPath.row]
         return cell
     }
     
-    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         let barHeight = Float(self.bar.frame.size.height)
         let viewHeight = Float(self.view.frame.height)
         let rowHeight = (viewHeight - barHeight) / Float(categoryArray.count)
@@ -51,28 +50,28 @@ class ViewController_chooseCategory: UIViewController, UITableViewDataSource, UI
         }
     }
     
-    // MARK: Methods to transition to another view controller
+    // MARK: Transition to Other VC
     
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        self.performSegueWithIdentifier("chosen", sender: self)
-        tableView.deselectRowAtIndexPath(indexPath, animated: false)
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "chosen", sender: self)
+        tableView.deselectRow(at: indexPath as IndexPath, animated: false)
     }
 
     @IBAction func cancel(sender: UIButton) {
-        self.performSegueWithIdentifier("chosen", sender: self)
+        self.performSegue(withIdentifier: "chosen", sender: self)
     }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "chosen") {
-            var upcoming: ViewController_Post = segue.destinationViewController as! ViewController_Post
-            if (self.tableView.indexPathForSelectedRow() != nil) {
-                let indexPath = self.tableView.indexPathForSelectedRow()!
+            let upcoming: ViewController_Post = segue.destination as! ViewController_Post
+            if (self.tableView.indexPathForSelectedRow != nil) {
+                let indexPath = self.tableView.indexPathForSelectedRow!
                 
                 let title = self.categoryArray[indexPath.row]
                 
                 upcoming.chosenCategory = title
                 
-                self.tableView.deselectRowAtIndexPath(indexPath, animated: true)
+                self.tableView.deselectRow(at: indexPath, animated: true)
             } else {
                 upcoming.chosenCategory = self.oldCategory!
             }
