e85cf809f24cb504be05c9fac340edee950a9a73
Michael Manhard <mmanhard93@gmail.com>
2020-02-15 15:02:31 -0600

    Add basic authorization.

--- backend_code/auth.py
+++ backend_code/auth.py
@@ -1,13 +1,28 @@
-###########################################################################
-# Require authorization
-###########################################################################
-# auth = HTTPBasicAuth()
-# @auth.get_password
-# def get_password(username):
-#     if username == '~MrG<.Lgz9}sBQL':
-#         return 'mcn^S4R4J`3v-(V5'
-#     return None
-#
-# @auth.error_handler
-# def unauthorized():
-#     return 'Unauthorized Access'
+from flask import request, make_response, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
+from models import User
+from database import db
+
+def verify_password(username, password):
+
+    return True
+    user = db.session.query(User).filter_by(username=username)
+    if user is None:
+        return False
+    else:
+        return check_password_hash(username, password)
+
+def auth_required(f):
+
+    @wraps(f)
+    def auth_wrapper(*args, **kwargs):
+        auth = request.authorization
+        username = auth.username
+        if (auth is None) or (auth.username is None) or (auth.password is None):
+            return make_response('Could not verify!', 401, {'WWW-Authenticate' : 'Basic realm="Login Required"'})
+        elif not verify_password(username, auth.password):
+            return make_response('Could not verify!', 401, {'WWW-Authenticate' : 'Basic realm="Incorrect username"'})
+        else:
+            return f(*args, **kwargs)
+    return auth_wrapper
