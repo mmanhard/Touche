8e263bf857e7f68972247923fe9d2dfae594f04a
Michael Manhard <mmanhard93@gmail.com>
2020-09-01 13:22:48 -0500

    -Add additional commentary to main view controller.

--- frontend_code/Touche/ViewControllers/ViewController.swift
+++ frontend_code/Touche/ViewControllers/ViewController.swift
@@ -27,33 +27,35 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var currentLocation: CLLocation!
     var currentUser: User?
     
-    // MARK: Methods to setup current view
+    // MARK: Lifecycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.currentCategory.text = self.categoryString
         
+        // Set the profile image and resize it.
         let image = UIImage(named:"profile.png") as UIImage?
         let size = CGSize(width: 22, height: 22)
         self.profile.setImage(Utility.RBResizeImage(image: image!, targetSize: size), for: .normal)
-        
-        self.tableView.rowHeight = UITableView.automaticDimension
         
         // Configure the location manage delegate.
         self.locationManager.delegate = self
         self.locationManager.desiredAccuracy = Constants.desiredLocAccuracy
         self.locationManager.requestWhenInUseAuthorization()
         
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = 160.0
+        // Configure the table view.
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 160.0
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        
+        // Make sure there is a user. Otherwise, go to login view.
         self.currentUser = User.getCurrentUser()
-        if self.currentUser != nil {
-            print("Logged In")
-        } else {
+        if self.currentUser == nil {
             self.performSegue(withIdentifier: "needsLogin", sender: self)
+            return
         }
         
         self.currentCategory.text = self.categoryString
@@ -69,6 +71,36 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             self.performSegue(withIdentifier: "locationServicesDisabled", sender: self)
         }
     }
+    
+    // MARK: Location Manager Methods
+    
+    // Listen for changes to the location services authorization status. If authorized, start updating locations, set the current location, and update the table view.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if (status == CLAuthorizationStatus.authorizedWhenInUse) {
+            self.locationManager.startUpdatingLocation()
+            currentLocation = self.locationManager.location
+            updateTable()
+        }
+    }
+    
+    // Listen for location errors. If error is of type CLError.denied, the user has disabled location services, so we segue to the no location services view.
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        if let clErr = error as? CLError {
+            switch clErr {
+                case CLError.denied:
+                    self.performSegue(withIdentifier: "locationServicesDisabled", sender: self)
+                default:
+                    print("other Core Location error")
+            }
+        }
+    }
+    
+    // Listen for updates to the current location.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations[0]
+    }
+
+    // MARK: TableView Methods
     
     // Updates the table by reloading questions from the backend and reloading the table view.
     func updateTable() {
@@ -101,35 +133,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
-    // MARK: CLLocationManagerDelegate Methods
-    
-    // Listen for changes to the location services authorization status. If authorized, start updating locations, set the current location, and update the table view.
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        if (status == CLAuthorizationStatus.authorizedWhenInUse) {
-            self.locationManager.startUpdatingLocation()
-            currentLocation = self.locationManager.location
-            updateTable()
-        }
-    }
-    
-    // Listen for location errors. If error is of type CLError.denied, the user has disabled location services, so we segue to the no location services view.
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        if let clErr = error as? CLError {
-            switch clErr {
-                case CLError.denied:
-                    self.performSegue(withIdentifier: "locationServicesDisabled", sender: self)
-                default:
-                    print("other Core Location error")
-            }
-        }
-    }
-    
-    // Listen for updates to the current location.
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        currentLocation = locations[0]
-    }
-
-    // MARK: UITableViewDataSource Methods
     // Determine the number of questions.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.questions.count
@@ -141,6 +144,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let cell = tableView.dequeueReusableCell(withIdentifier: "questionCell", for: indexPath) as! TableViewCell_Question
         let question = self.questions[indexPath.row]
         
+        // Get all display information from the question object.
         cell.questionLabel.text = question.question
         cell.timeLabel.text = getTime(timeDifference: question.datetime)
         cell.Answers = question.answers as NSArray
@@ -148,6 +152,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.numVote = question.total_votes
         cell.qCategory = question.category
         
+        // Get the number of votes and format the votes string.
         let numVotes = question.total_votes
         if numVotes == 1 {
             cell.voteCountLabel.text = "\(numVotes) vote"
@@ -158,18 +163,21 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
 
-    // MARK: Methods to transition to another view controller
+    // MARK: View Transition Methods
     
+    // Handler for selecting select category. Shows the select category view.
     @IBAction func selectCategory(with sender: UIButton) {
         self.performSegue(withIdentifier: "selectCategory", sender: self)
     }
     
+    // Handler for selecting a row of the question table. Shows the view for the corresponding question.
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.questionToPass = self.questions[indexPath.row]
         self.performSegue(withIdentifier: "viewQuestionFromHome", sender: self)
         tableView.deselectRow(at: indexPath as IndexPath, animated: false)
     }
     
+    // Handler for preparing to segue to another view. Primarily used to pass data between view controllers.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
     {
         if (segue.identifier == "selectCategory") {
@@ -186,7 +194,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         self.locationManager.stopUpdatingLocation()
     }
     
-    // MARK: Miscellaneous methods
+    // MARK: Misc. methods
 
     // Changes the table to either be sorted by how recent they are or how popular they are.
     @IBAction func changeSorting(with sender: UISegmentedControl) {
