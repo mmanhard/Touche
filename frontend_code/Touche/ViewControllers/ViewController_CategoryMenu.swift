75a127e18784a1d7c0e839dd3f51ac8a53681de6
Michael Manhard <mmanhard93@gmail.com>
2020-09-01 14:50:24 -0500

    -Add additional commentary to category view controllers.

--- frontend_code/Touche/ViewControllers/ViewController_CategoryMenu.swift
+++ frontend_code/Touche/ViewControllers/ViewController_CategoryMenu.swift
@@ -26,11 +26,12 @@ class ViewController_CategoryMenu: UIViewController, UICollectionViewDelegateFlo
     bottom: 10.0,
     right: 20.0)
     
-    // MARK: Methods to setup current view
+    // MARK: Lifecycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Set the profile image and resize it.
         let image = UIImage(named:"touche_icon.png") as UIImage?
         let size = CGSize(width: 36, height: 36)
         self.homeButton.setImage(Utility.RBResizeImage(image: image!, targetSize: size), for: UIControl.State.normal)
@@ -38,12 +39,14 @@ class ViewController_CategoryMenu: UIViewController, UICollectionViewDelegateFlo
         self.collectionView.reloadData()
     }
     
-    // MARK: UITableViewDataSource and UITableViewDelegate Methods
+    // MARK: Collection View Methods
     
+    // Determine the number of sections (2 categories per section).
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return (Constants.availCategories.count / 2 + Constants.availCategories.count % 2)
     }
     
+    // Determine the number of items in a section (2 unless at less section and number of sections is odd).
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if section < (Constants.availCategories.count / 2) {
             return 2
@@ -52,25 +55,33 @@ class ViewController_CategoryMenu: UIViewController, UICollectionViewDelegateFlo
         }
     }
     
+    // Populate each row of the category table.
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: "categoryCell", for: indexPath) as! CollectionViewCell
     
+        // Set the category text.
         let categoryNum = 2 * (indexPath.section) + indexPath.row
         cell.titleLabel.text = Constants.availCategories[categoryNum]
+        
+        // Update display of cell.
         cell.layer.cornerRadius = 10
         cell.layer.borderColor = CGColor(srgbRed: 1.0, green: 0.0, blue: 0.0, alpha: 0.7)
         cell.layer.borderWidth = 5
         return cell
     }
     
+    // Determine the size of the collection view item at the given index path.
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        // Determine the width of the item.
         let numItemsInSection = CGFloat(collectionView.numberOfItems(inSection: indexPath.section))
         let paddingSpaceX = sectionInsets.left * (numItemsInSection + 1)
         let availableWidth = collectionView.frame.width - paddingSpaceX
         let widthPerItem = availableWidth / numItemsInSection
         
+        // Determine the height of the item.
         let numSections = CGFloat(collectionView.numberOfSections)
         let paddingSpaceY = (sectionInsets.bottom + sectionInsets.top) * numSections
         let availableHeight = collectionView.frame.height - paddingSpaceY
@@ -79,31 +90,38 @@ class ViewController_CategoryMenu: UIViewController, UICollectionViewDelegateFlo
         return CGSize(width: widthPerItem, height: heightPerItem)
     }
     
+    // Determine the insets for each section.
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         insetForSectionAt section: Int) -> UIEdgeInsets {
       return sectionInsets
     }
     
+    // Determine the minimum line spacing for each section.
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         minimumLineSpacingForSectionAt section: Int) -> CGFloat {
       return sectionInsets.left
     }
     
+    // Handler for selecting a given category.
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let rootVC = self.navigationController?.viewControllers[0] as! ViewController
         
+        // Get the category of the selected cell, set the category string of the root
+        // view controller equal to it, and deselect the cell.
         let categoryNum = 2 * (indexPath.section) + indexPath.row
         rootVC.categoryString = Constants.availCategories[categoryNum]
         collectionView.deselectItem(at: indexPath, animated: true)
         
+        // Transition back to the most recent view controller.
         self.navigationController?.popViewController(animated: true)
         self.dismiss(animated: true, completion: nil)
     }
     
-    // MARK: Methods to transition to another view controller
+    // MARK: Transition Methods
     
+    // Handler for selecting cancel button. Transitions back to the most recent view controller.
     @IBAction func cancelCategorySelection(sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
         self.dismiss(animated: true, completion: nil)
--- frontend_code/Touche/ViewControllers/ViewController_chooseCategory.swift
+++ frontend_code/Touche/ViewControllers/ViewController_chooseCategory.swift
@@ -26,15 +26,14 @@ class ViewController_chooseCategory: UIViewController, UICollectionViewDataSourc
     bottom: 10.0,
     right: 20.0)
     
-    // MARK: UITableViewDataSource and UITableViewDelegate Methods
-
+    // MARK: Collection View Methods
     
-    // MARK: Transition to Other VC
-    
+    // Determine the number of sections (2 categories per section).
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return (Constants.availCategories.count / 2 + Constants.availCategories.count % 2)
     }
     
+    // Determine the number of items in a section (2 unless at less section and number of sections is odd).
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if section < (Constants.availCategories.count / 2) {
             return 2
@@ -43,26 +42,34 @@ class ViewController_chooseCategory: UIViewController, UICollectionViewDataSourc
         }
     }
     
+    // Populate each row of the category table.
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: "categoryCell", for: indexPath) as! CollectionViewCell
     
+        // Set the category text.
         let categoryNum = 2 * (indexPath.section) + indexPath.row
         cell.titleLabel.text = Constants.availCategories[categoryNum]
+        
+        // Update display of the cell.
         cell.layer.cornerRadius = 10
         cell.layer.borderColor = CGColor(srgbRed: 1.0, green: 0.0, blue: 0.0, alpha: 0.7)
         cell.layer.borderWidth = 5
         return cell
     }
     
+    // Determine the size of the collection view item at the given index path.
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        // Determine the width of the item.
         let numItemsInSection = CGFloat(collectionView.numberOfItems(inSection: indexPath.section))
         let paddingSpaceX = sectionInsets.left * (numItemsInSection + 1)
         let availableWidth = collectionView.frame.width - paddingSpaceX
         print(availableWidth)
         let widthPerItem = availableWidth / numItemsInSection
         
+        // Determine the height of the item.
         let numSections = CGFloat(collectionView.numberOfSections)
         let paddingSpaceY = sectionInsets.bottom * (numSections+1)
         let availableHeight = collectionView.frame.height - paddingSpaceY
@@ -71,21 +78,27 @@ class ViewController_chooseCategory: UIViewController, UICollectionViewDataSourc
         return CGSize(width: widthPerItem, height: heightPerItem)
     }
     
+    // Determine the insets for each section.
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         insetForSectionAt section: Int) -> UIEdgeInsets {
       return sectionInsets
     }
     
+    // Determine the minimum line spacing for each section.
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         minimumLineSpacingForSectionAt section: Int) -> CGFloat {
       return sectionInsets.left
     }
     
+    // Handler for selecting a given category.
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let numVCs = self.navigationController?.viewControllers.count
         let nextVC = self.navigationController?.viewControllers[numVCs! - 2] as! ViewController_Post
+        
+        // Get the category of the selected cell, set the category string of the root
+        // view controller equal to it, and deselect the cell.
         let categoryNum = 2 * (indexPath.section) + indexPath.row
         nextVC.chosenCategory = Constants.availCategories[categoryNum]
         collectionView.deselectItem(at: indexPath, animated: true)
@@ -94,6 +107,9 @@ class ViewController_chooseCategory: UIViewController, UICollectionViewDataSourc
         self.dismiss(animated: true, completion: nil)
     }
 
+    // MARK: Transition Methods
+    
+    // Handler for selecting cancel button. Transitions back to the most recent view controller.
     @IBAction func cancel(with sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
         self.dismiss(animated: true, completion: nil)
